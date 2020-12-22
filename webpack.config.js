f949ea08eee0f7bb273b850816dea9d6277737e0
Niklas Barning <niklas.barning@hmmh.de>
2020-12-22 13:43:28 +0100

    Fix babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const babelConfig = require('./babel.config.json');
+
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -25,6 +27,24 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+
+        // See https://stackoverflow.com/questions/57361439/how-to-exclude-core-js-using-usebuiltins-usage/59647913#59647913
+        // and https://github.com/babel/babel-loader#exclude-libraries-that-should-not-be-transpiled
+        exclude: [
+          /\bcore-js\b/,
+          /\bwebpack\/buildin\b/
+        ],
+
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            ...babelConfig
+          }
+        }
+      },      
       {
         loader: 'babel-loader',
         test: /\.js$/,
