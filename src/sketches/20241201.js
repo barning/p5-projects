41b034a0b240f9f5964065962f0276d0b932ec7a
Niklas <nb@niklasbarning.de>
2024-12-22 21:51:54 +0100

    Refactored code

--- src/sketches/20241201.js
+++ src/sketches/20241201.js
@@ -6,17 +6,23 @@ export default function sketch(s) {
     s.draw = () => {
         let startcolor = s.color(179, 200, 216);
         let endcolor = s.color(250, 142, 75);
-
         let blockSteps = s.height / 10;
 
+        drawGradientBlocks(s, startcolor, endcolor, blockSteps);
+        drawMiddleStripes(s, endcolor, startcolor, blockSteps);
+    };
+
+    function drawGradientBlocks(s, startcolor, endcolor, blockSteps) {
         for (let y = 0; y < s.height; y += blockSteps) {
             s.fill(s.lerpColor(startcolor, endcolor, s.map(y, 0, s.height, 0.1, 1)));
             s.noStroke();
             s.rect(0, y, s.width, blockSteps);
         }
+    }
 
+    function drawMiddleStripes(s, startcolor, endcolor, blockSteps) {
         for (let y = blockSteps * 2; y < blockSteps * 8; y += 1) {
-            s.fill(s.lerpColor(endcolor, startcolor, s.map(y, 0, blockSteps * 8, 0.1, 1)));
+            s.fill(s.lerpColor(endcolor, startcolor, s.map(y, blockSteps * 2, blockSteps * 8, 0.1, 1)));
             s.noStroke();
             s.rect(s.width / 3, y, s.width / 3, 1);
         }
